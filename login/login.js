6af29a6b9d2a523ec08e1e8bc8788675b4b72e41
SwatiHD <swatihd1208@gmail.com>
2024-12-20 19:15:09 +0530

    first login

---
+++ login/login.js
@@ -0,0 +1,11 @@
+document.querySelector('form').addEventListener('submit', (event)=>{
+    event.preventDefault();
+     const username = document.querySelector('#username').value;
+     const password = document.querySelector('#password').value;
+
+     if(username === 'swati' && password==='12345'){
+        alert("success");
+     }else{
+        alert("incorrect")
+     }
+})
